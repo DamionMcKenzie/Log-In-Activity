27b6e165e792124657e08b544991723c713c0e52
Damion-Mckenzie <damionmckenzie133@gmail.com>
2018-11-10 01:57:01 -0600

    Initial commit

---
+++ app/src/main/java/com/damionmckenzie/log_in_activity/MainActivity.java
@@ -0,0 +1,13 @@
+package com.damionmckenzie.log_in_activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
